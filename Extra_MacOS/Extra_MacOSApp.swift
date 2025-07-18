0e673a23b0042fe88b43482dcfd4f1b98958b35f
EverLegacy <adrian.200256600.am@gmail.com>
2025-07-18 01:49:20 -0500

    Initial Commit

---
+++ Extra_MacOS/Extra_MacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Extra_MacOSApp.swift
+//  Extra_MacOS
+//
+//  Created by Romel Ruiz on 7/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Extra_MacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Extra_MacOSTests/Extra_MacOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Extra_MacOSTests.swift
+//  Extra_MacOSTests
+//
+//  Created by Romel Ruiz on 7/18/25.
+//
+
+import Testing
+@testable import Extra_MacOS
+
+struct Extra_MacOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
